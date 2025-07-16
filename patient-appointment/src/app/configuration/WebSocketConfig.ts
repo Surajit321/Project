dd01381bc4d51de57833b8ad75beecc09ee2398e
Surajit Borah <surajitsb.borah@gmail.com>
2025-07-16 19:45:23 +0530

    fixed websocket url

--- patient-appointment/src/app/configuration/WebSocketConfig.ts
+++ patient-appointment/src/app/configuration/WebSocketConfig.ts
@@ -1,10 +1,12 @@
 import { RxStomp } from '@stomp/rx-stomp';
 
+// const LOCAL_BACKEND_URL = 'ws://localhost:8080/ws';
+const BACKEND_URL = 'ws://patientappointmentservice.onrender.com/ws';
 export const createStompClient = () => {
   const rxStomp = new RxStomp();
 
   rxStomp.configure({
-    brokerURL: 'ws://localhost:8080/ws',
+    brokerURL: `ws://${BACKEND_URL}/ws`,
     connectHeaders: {},
     heartbeatIncoming: 0,
     heartbeatOutgoing: 20000,
